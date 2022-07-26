b86a9ffc3acebd364d5418aa7cf5dc4e644ac3d5
alsabagh-dev <zizo.naser@stud.fci-cu.edu.eg>
2022-07-26 17:24:19 +0200

    fix pass env variables to eb

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,12 +1,19 @@
 # Choose the enviroment
 eb use udagram-api-dev 
 
-# I know I can pass env variables t EB
-#  but for this project circumstances 
-# I choose to add them to Eb Manually
 
 # Set env  variables
-eb setenv APP_PORT=8080
+eb setenv APP_PORT=$APP_PORT
+eb setenv AWS_BUCKET=$AWS_BUCKET
+eb setenv AWS_PROFILE=$AWS_PROFILE
+eb setenv AWS_REGION=$AWS_REGION
+eb setenv JWT_SECRET=$JWT_SECRET
+eb setenv POSTGRES_DB=$POSTGRES_DB
+eb setenv POSTGRES_HOST=$POSTGRES_HOST
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
+eb setenv URL=$URL
+eb setenv DB_PORT=$DB_PORT
 
 
 # Deploy
